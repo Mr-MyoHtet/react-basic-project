4e4d3a27c6416b1b51c7ad63f73b0186228f72ed
myohtet <52666491+Mr-MyoHtet@users.noreply.github.com>
2023-12-19 18:09:08 +0630

    steps layout

--- steps/src/App.js
+++ steps/src/App.js
@@ -1,10 +1,27 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+const messages = ["Learn React", "Apply Job", "Get Income"];
 function App() {
+  const step = 1;
   return (
-    <div>
-      <p>Hello world</p>
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+      <p className="message">
+        Step{step} : {messages[step - 1]}
+      </p>
+      <div className="buttons">
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Previous
+        </button>
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Next
+        </button>
+      </div>
     </div>
   );
 }
