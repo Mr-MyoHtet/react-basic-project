55eed580a0a317ca35d4097a40e051ec0436a4ea
myohtet <52666491+Mr-MyoHtet@users.noreply.github.com>
2023-12-20 18:08:39 +0630

    clsoe button and react developer extension

--- steps/src/App.js
+++ steps/src/App.js
@@ -5,6 +5,7 @@ import { useState } from "react";
 const messages = ["Learn React", "Apply Job", "Get Income"];
 function App() {
   const [step, setState] = useState(1);
+  const [isOpen, setisOpen] = useState(true);
 
   function handlePrevious() {
     if (step > 1) setState(step - 1);
@@ -12,38 +13,48 @@ function App() {
   function handleNext() {
     if (step < 3) setState(step + 1);
   }
+  function openCLose() {
+    setisOpen(!isOpen);
+  }
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
-      </div>
-      <p className="message">
-        Step{step} : {messages[step - 1]}
-      </p>
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handlePrevious}
-        >
-          {/* comment start
-          handlePrevious is passing function value ,it not function call 
-          comment end
-          */}
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handleNext}
-        >
-          {/* comment start
-          handleNext is passing function value ,it not function call 
-          comment end
-          */}
-          Next
-        </button>
-      </div>
+    <div>
+      <button className="close" onClick={openCLose}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+          </div>
+          <p className="message">
+            Step{step} : {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              {/* comment start
+      handlePrevious is passing function value ,it not function call 
+      comment end
+    */}
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              {/* comment start
+    handleNext is passing function value ,it not function call 
+    comment end
+  */}
+              Next
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
